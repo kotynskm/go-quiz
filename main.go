fabccdc494dda838ab69b94ae1a307d535ae07a9
Kailey <k.kotynski@gmail.com>
2023-05-01 16:24:36 -0700

    Added check for user input

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strconv"
 )
 
 // open file and read contents
@@ -31,12 +32,25 @@ func giveQuiz(quizFile [][]string){
 		fmt.Printf("Question #%d: %s Enter your answer --> ",i + 1, question[0])
 
 		// get user input
-		var userAnswer string
-		fmt.Scanln(&userAnswer)
-		// check if answer is correct
-		if userAnswer == question[1] {
-			totalScore++
+		var userAnswer int
+
+		for {
+			_, err := fmt.Scanf("%d", &userAnswer)
+				if err == nil {
+					break;
+				}
+				fmt.Println("not valid answer, try again")
+				var dump string
+				fmt.Scanln(&dump)
+			
 		}
+		
+		// check if answer is correct
+		intAnswer, _ := strconv.Atoi(question[1])
+		if userAnswer == intAnswer{
+			totalScore++
+		} 
+		
 	}
 	fmt.Printf("You got a total of %d correct answers", totalScore)
 }
